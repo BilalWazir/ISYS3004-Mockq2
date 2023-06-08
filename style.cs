893c8cd2c7cc136eb4c697804704ea293e69023d
BilalWazir <101708971+BilalWazir@users.noreply.github.com>
2023-06-08 17:56:34 +0800

    Add files via upload

---
+++ style.cs
@@ -0,0 +1,12 @@
+body {
+    font-family: Arial, sans-serif;
+    background-color: #f5f5f5;
+    padding: 20px;
+    color: #333;
+}
+
+#weatherWarnings {
+    border: 1px solid #ddd;
+    padding: 10px;
+    border-radius: 5px;
+}
